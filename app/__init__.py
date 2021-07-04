b98eb41074c46b4e7b4f37981b39ee7eabe4e62e
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2021-07-04 19:35:45 -0400

    edit login and register endpoints, hopefully that does the trick on the CentOS instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from app.db import get_db
-#Response, send_from_directory, redirect
-#from dotenv import load_dotenv
-#from . import db
 
 #load_dotenv()
 app = Flask(__name__)
@@ -63,9 +59,7 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
@@ -93,17 +87,13 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
