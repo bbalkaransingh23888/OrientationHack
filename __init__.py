1c4c64ea2a2897f7eb46e1f4c7351a851288cb3c
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2021-07-09 14:40:11 -0400

    Updates

--- __init__.py
+++ __init__.py
@@ -1,9 +1,35 @@
 import os
-from flask import Flask, render_template, send_from_directory, request, redirect
-from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-load_dotenv()
+#load_dotenv()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 
 @app.route('/')
@@ -21,3 +47,58 @@ def blog():
 @app.route('/projects')
 def projects():
     return render_template('projectsPage.html', title="John Smith", url=os.getenv("URL"))
+
+@app.route('/health')
+def healthy():
+    return "This app works"
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user.password, password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    #return "Login page not yet implemented", 501
+    return render_template('login.html', title="John Smith", url=os.getenv("URL")), 200
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif UserModel.query.filter_by(username=username).first() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    # return "Register Page not yet implemented", 501
+    return render_template('register.html', title="John Smith", url=os.getenv("URL")), 200
+
